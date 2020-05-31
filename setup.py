04af030ef8ef63988f612b3be7364aa8a562c72c
sorenmulli <swholm@protonmail.com>
2020-05-31 03:49:45 +0200

    Sane versioning

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
 	name='pelutils',
-	version='0.0.1a',
+	version='0.0.1-1',
 	description='Utility functions that are commmonly useful',
 	long_description_content_type="text/markdown",
 	long_description=README + '\n\n' + HISTORY,
