e88478c4a6004b1608741ecc03c88ce3726cb7fc
Asger Schultz <asger.s@protonmail.com>
2021-01-18 16:27:02 +0100

    Add torch functions to ds

--- pelutils/ds/__init__.py
+++ pelutils/ds/__init__.py
@@ -1,4 +1,5 @@
 import functools
+from typing import Type
 
 _import_error = ModuleNotFoundError("To use the ds submodule, you must install pelutils[ds]")
 
@@ -30,4 +31,53 @@ def no_grad(fun):
     return wrapper
 
 
+class BatchFeedForward:
+    """
+    This class handles feedforwarding large batches that would otherwise cause memory overflow
+    It works by splitting it into smaller batches, if it encounters a memory error
+    Only works when gradient should not be tracked
+    """
+
+    def __init__(self, net: Type[torch.nn.Module], data_points: int, increase_factor=2):
+        """
+        net: torch network
+        data_points: Number of data points in each feed forward
+        increase_factor: Multiply number of batches with this each time a memory error occurs
+        """
+        self.net = net
+        self.data_points = data_points
+        self.increase_factor = increase_factor
+        self.batches = 1
+
+    @no_grad
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        while True:
+            try:
+                output_parts = [self.net(x[slice_]) for slice_ in self._get_slices()]
+                output = torch.cat(output_parts)
+                break
+            # Usually caused by running out of vram. If not, the error is still raised, else batch size is reduced
+            except RuntimeError as e:
+                if "alloc" not in str(e):
+                    raise e
+                self._more_batches()
+        return output
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.forward(x)
+
+    def update_net(self, net: Type[torch.nn.Module]):
+        self.net = net
+
+    def _more_batches(self):
+        self.batches *= self.increase_factor
+
+    def _get_slices(self):
+        slice_size = self.data_points // self.batches + 1
+        # Final slice may have overflow, however this is simply ignored when indexing
+        slices = [slice(i*slice_size, (i+1)*slice_size) for i in range(self.batches)]
+        return slices
+
+
 from . import plot
+from . import stats
