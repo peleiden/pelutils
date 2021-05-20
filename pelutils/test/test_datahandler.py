10bcf0c8abbb7b3f427bea9a018dff00f4cb8c80
Asger Schultz <asger.s@protonmail.com>
2021-05-20 23:30:18 +0200

    Add ignore_missing field to DataStorage
    This is useful for backwards compatibility

--- pelutils/test/test_datahandler.py
+++ pelutils/test/test_datahandler.py
@@ -3,6 +3,7 @@ import os
 from dataclasses import dataclass
 
 import numpy as np
+import pytest
 import torch
 
 from pelutils import MainTest, DataStorage
@@ -15,15 +16,36 @@ class T(DataStorage):
     c: str
     d: dict[tuple[int], int]
 
+@dataclass
+class TExtra(T):
+    e: np.ndarray
+    f: float
+
+@dataclass
+class TExtraIgnore(T):
+    e: np.ndarray
+    f: float
+    ignore_missing = True
+
 
 class TestDatahandler(MainTest):
+
+    data = {"a": np.array([1]), "b": torch.Tensor([1]), "c": "lala", "d": {(1,2): 1}}
+
     def test_saveload(self):
         # Use dict data that is not json serializable
-        datas = {"a": np.array([1]), "b": torch.Tensor([1]), "c": "lala", "d": {(1,2): 1}}
-        t = T(**datas)
+        t = T(**self.data)
         t.save(self.test_dir)
         for f in ("a.npy", "b.pt", "data.json", "d.p"):
             assert os.path.isfile(os.path.join(self.test_dir, f))
         t = T.load(self.test_dir)
-        for n, d in datas.items():
+        for n, d in self.data.items():
             assert getattr(t, n) == d
+
+    def test_ignore_missing(self):
+        t = T(**self.data)
+        t.save(self.test_dir)
+        with pytest.raises(TypeError):
+            TExtra.load(self.test_dir)
+        t = TExtraIgnore.load(self.test_dir)
+        assert t.e is None and t.f is None
