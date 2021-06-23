5e479aac39c7f3fba48e5e96bb3305a397351564
Asger Schultz <asger.s@protonmail.com>
2021-06-23 22:42:02 +0200

    Add linecounter entry point
    Resolves #62

---
+++ pelutils/ds/_linecounter.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+import os
+import time
+
+import click
+import git
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from pelutils import get_repo
+from pelutils.ds.plots import figsize_std, update_rc_params, rc_params_small
+
+
+_default_extensions = ", ".join((
+    ".py", ".pyw",
+    ".c", ".cpp", ".cc", ".h",
+    ".tex",
+    ".html", ".css", ".js", ".jsx", ".ts",
+    ".java", ".kt", ".dart", ".swift",
+    ".rs", ".go",
+    ".r", ".m",
+))
+
+def _count(repo: git.Repo, branch: git.Head, exts: list[str]) -> tuple[np.ndarray, dict[str, np.ndarray]]:
+    """ Counts lines of all files recursively in the current working directory
+    Returns an array of commit epoch times and a dict mapping file extensions to line counts """
+    commits = list(reversed(list(repo.iter_commits())))  # List of commit from oldest to newest
+    times = np.array([c.committed_date for c in commits])
+    lines = { ext: np.zeros_like(times) for ext in exts }
+    for i, commit in enumerate(tqdm(commits)):
+        try:
+            repo.git.checkout(str(commit))
+        except git.GitCommandError as e:
+            # This error happens if there is a file that has been moved in and out of .gitignore
+            # For safety, the commit is ignored
+            if "error: The following untracked working tree files would be overwritten by checkout:" in str(e):
+                continue
+            raise
+        for path, __ in repo.index.entries:
+            try:
+                ext = next(e for e in exts if path.endswith(e) and os.path.isfile(path))
+            except StopIteration:
+                continue
+            with open(path) as f:
+                lines[ext][i] += len([line for line in f.readlines() if line.strip()])
+
+    repo.git.checkout(branch)
+    return times, lines
+
+def _fuse_times(all_times: list[np.ndarray]) -> np.ndarray:
+    """ Merges all time arrays together into a single array that is also sorted """
+    n = sum(times.size for times in all_times)
+    times = np.empty(n, dtype=int)
+    idcs = [0] * len(all_times)
+    for i in range(n):
+        current_times = [time[idx] if idx < len(time) else float("inf") for time, idx in zip(all_times, idcs)]
+        min_time = np.argmin(current_times)
+        times[i] = current_times[min_time]
+        idcs[min_time] += 1
+    return times
+
+def _last_initial_zero(values: np.ndarray) -> int:
+    if values[0] != 0:
+        return 0
+    return np.where(values!=0)[0][0] - 1
+
+@click.command()
+@click.argument("repos", nargs=-1)
+@click.option("-e", "--extensions", default=_default_extensions, help="Comma seperated list of file extensions to look for")
+@click.option("-o", "--outplot-path", default="linecount.png")
+@click.option("-d", "--date-format", default="%d-%m-%y", help="How to format axis labels")
+@click.option("-n", "--no-repo-name", is_flag=True)
+def linecounter(repos: list[str], extensions: str, outplot_path: str, date_format: str, no_repo_name: bool):
+    extensions = [x.strip() for x in extensions.split(",")]
+    extensions = [x if x.startswith(".") else "." + x for x in extensions]
+    wd = os.getcwd()
+    repo_names, all_times, all_counts = list(), list(), list()
+    for repo in repos:
+        repo_path, __ = get_repo(repo)
+        if repo_path is None:
+            raise ValueError("%s is not a git repository" % repo_path)
+        os.chdir(repo_path)
+
+        try:
+            repo = git.Repo()
+            branch = repo.active_branch
+            times, lines = _count(repo, branch, extensions)
+        except:
+            # Reset repo
+            repo.git.checkout(branch)
+            raise
+
+        repo_names.append(os.path.split(repo_path)[-1])
+        all_times.append(times)
+        all_counts.append(lines)
+        os.chdir(wd)
+
+    plt.figure(figsize=figsize_std)
+    for i, (repo_name, times, counts) in enumerate(zip(repo_names, all_times, all_counts)):
+        for ext, l in counts.items():
+            if not l.any():
+                continue
+            non_zero = l != 0
+            non_zero[_last_initial_zero(l)] = 1
+            all_times[i] = times[non_zero]
+            all_counts[i] = l[non_zero]
+            lab = ext+(f" ({repo_name})" if len(repo_names) > 1 and not no_repo_name else "")
+            plt.plot(all_times[i], all_counts[i], marker=".", ms=8, lw=2, label=lab)
+
+    fused_times = _fuse_times(all_times)
+    xticks = np.linspace(0, len(fused_times)-1, 10, dtype=int)
+    xticklabels = [time.strftime(date_format, time.gmtime(x)) for i, x in enumerate(fused_times) if i in xticks]
+    plt.xticks([fused_times[i] for i in xticks], xticklabels, rotation=60)
+    title = "Line counts over time" + (f" ({repo_name})" if len(repo_names) == 1 and not no_repo_name else "")
+    plt.title(title)
+    plt.xlabel("Date of commit")
+    plt.ylabel("Number of lines (excluding empty lines)")
+    plt.legend(loc=2)
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(outplot_path)
+
+
+if __name__ == "__main__":
+    update_rc_params(rc_params_small)
+    linecounter()
+
