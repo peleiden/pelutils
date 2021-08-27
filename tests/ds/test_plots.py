46d8e19feeaed46ce82089f1919b400fee2204f1
Asger Schultz <asger.s@protonmail.com>
2021-08-27 23:25:31 +0200

    Add tests for binning functions

---
+++ tests/ds/test_plots.py
@@ -0,0 +1,31 @@
+from math import ceil
+
+import numpy as np
+
+from pelutils.tests import MainTest
+from pelutils.ds.plots import linear_binning, log_binning, normal_binning
+
+
+class TestBinning:
+
+    bins = 25
+    uniform_data = np.random.uniform(1, 200, 500)
+
+    def test_linear_binning(self):
+        binning = linear_binning(self.uniform_data, self.bins)
+        diffs = np.diff(binning)
+        for a, b in zip(diffs[:-1], diffs[1:]):
+            assert np.isclose(a, b)
+
+    def test_log_binning(self):
+        binning = log_binning(self.uniform_data, self.bins)
+        ratios = binning[1:] / binning[:-1]
+        diffs = np.diff(ratios)
+        for a, b in zip(diffs[:-1], diffs[1:]):
+            assert np.isclose(a, b)
+
+    def test_normal_binning(self):
+        binning = normal_binning(self.uniform_data, self.bins)
+        for i in range(ceil(self.bins / 2)):
+            assert np.isclose(binning[i+1]-binning[i], binning[self.bins-i-1]-binning[self.bins-i-2])
+        assert (np.diff(np.diff(np.diff(binning))) > 0).all()
