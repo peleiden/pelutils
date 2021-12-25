e98ec69bbdd0ac9f9d9c3f9753b6548714c60072
Asger Schultz <asger.s@protonmail.com>
2021-12-25 20:41:51 +0100

    Build for all platforms on CI/CD
    Resolves #12
    
    Resolves #17

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-import subprocess
+from distutils.command.build import build as build_
+from distutils.core import Extension
 from setuptools import setup, find_packages
-from distutils.command.install import install as install_
+from setuptools.extension import Extension
+import subprocess
 
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
@@ -17,11 +19,10 @@ with open("README.md") as readme_file:
 with open("CHANGELOG.md") as history_file:
     CHANGELOG = history_file.read()
 
-class install(install_):
+class build(build_):
+
     def run(self):
         subprocess.call("git submodule update --init --recursive".split())
-        subprocess.call("make clean".split())
-        subprocess.call("make".split())
         super().run()
 
 setup_args = dict(
@@ -32,7 +33,7 @@ setup_args = dict(
     long_description = README + "\n\n" + CHANGELOG,
     license          = "BSD-3-Clause",
     packages         = find_packages(),
-    package_data     = { "pelutils": ["so/ds.so"] },
+    package_data     = { "pelutils": [ "ds/ds.c", "ds/hashmap.c/*" ] },
     author           = "Asger Laurits Schultz, Søren Winkel Holm",
     author_email     = "asger.s@protonmail.com;swholm@protonmail.com",
     keywords         = [ "utility", "logger", "parser", "profiling" ],
@@ -44,7 +45,8 @@ setup_args = dict(
         "linecounter = pelutils.ds._linecounter:linecounter",
         "pelexamples = examples.cli:run",
     ] },
-    cmdclass         = { "install": install },
+    cmdclass         = { "build": build },
+    ext_modules      = [ Extension("pelutils", ["pelutils/ds/ds.c"]) ],
 )
 
 if __name__ == "__main__":
