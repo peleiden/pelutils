4aad3690fe993558f30a7c71daff75548b164255
Asger Schultz <asger.s@protonmail.com>
2021-12-26 10:45:08 +0100

    Cast void pointers to allow compilation on Windows

--- pelutils/ds/ds.c
+++ pelutils/ds/ds.c
@@ -21,12 +21,13 @@ int compare(const void* elem1, const void* elem2, void* udata) {
     return memcmp(e1->p_elem, e2->p_elem, e1->stride);
 }
 
-size_t unique(size_t n,       // Number of array elements
-              size_t stride,  // Number of bytes between elements on primary axis
-              void* array,    // Non-empty, contiguous array of any shape
-              long* index,    // Array of size n to put unique values
-              long* inverse,  // Array of size n to put inverse values
-              long* counts    // Array of size n to put number of each unique element
+extern size_t unique(
+    size_t n,       // Number of array elements
+    size_t stride,  // Number of bytes between elements on primary axis
+    void* array,    // Non-empty, contiguous array of any shape
+    long* index,    // Array of size n to put unique values
+    long* inverse,  // Array of size n to put inverse values
+    long* counts    // Array of size n to put number of each unique element
 ) {
     if (index == NULL)
         return 0;
@@ -35,14 +36,14 @@ size_t unique(size_t n,       // Number of array elements
     for (size_t i = 0; i < n; i ++) {
         // Construct element
         struct elem this_elem = {
-            .p_elem = array + stride * i,
+            .p_elem = (char*)array + stride * i,
             .stride = stride,
         };
         // Check if already in map
         struct elem* p_found_elem = hashmap_get(map, &this_elem);
         if (p_found_elem != NULL) {
             // Get index of found element by difference in memory address
-            size_t found_index = (p_found_elem->p_elem - array) / stride;
+            size_t found_index = ((char*)(p_found_elem->p_elem) - (char*)array) / stride;
             if (inverse != NULL)
                 inverse[i] = inverse[found_index];
             if (counts != NULL)
