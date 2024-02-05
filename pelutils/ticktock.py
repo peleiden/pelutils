fb4d2905b98d27b5191268a59563966097b3c049
Asger Schultz <asger.s@protonmail.com>
2024-02-05 07:59:42 +0100

    Remove std from TickTock profiles

--- pelutils/ticktock.py
+++ pelutils/ticktock.py
@@ -54,12 +54,6 @@ class Profile:
         """ Returns mean runtime lengths """
         return self.sum() / len(self) if self._hits else 0
 
-    def std(self) -> float:
-        """ Returns empirical standard deviation of runtime
-        Be aware that this is highly sensitive to outliers and often a bad estimate """
-        s = self.mean()
-        return (1 / (len(self)+1) * sum(map(lambda x: (x-s)**2, self._hits))) ** 0.5
-
     def __str__(self) -> str:
         return self.name
 
@@ -246,7 +240,7 @@ class TickTock:
         str_ = f"{dt/unit[1]:.3f} {unit[0]}"
         return thousands_seperators(str_)
 
-    def stringify_sections(self, unit: tuple[str, float]=TimeUnits.second, with_std=False) -> str:
+    def stringify_sections(self, unit: tuple[str, float]=TimeUnits.second) -> str:
         """ Returns a pretty print of profiles """
         if self._profile_stack:
             raise ValueError("TickTock instance cannot be stringified while profiling is still ongoing. "\
@@ -254,8 +248,6 @@ class TickTock:
 
         table = Table()
         h = ["Profile", "Total time", "Percentage", "Hits", "Average"]
-        if with_std:
-            h.append("Std.")
         table.add_header(h)
         total_time = sum(p.sum() for p in self.profiles)
         for profile in self:
@@ -267,8 +259,6 @@ class TickTock:
                 thousands_seperators(len(profile)),
                 self.stringify_time(profile.mean(), TimeUnits.next_smaller(unit))
             ]
-            if with_std:
-                row.append(self.stringify_time(profile.std(), TimeUnits.next_smaller(unit)))
             table.add_row(row, [True] + [False] * (len(row)-1))
 
         return str(table)
