6f9ec0c2e9075b31b536791c1bd433993ecc296a
asgerius <asger.s@pm.me>
2026-05-03 17:12:30 +0200

    Add BoolArray and ComplexArray types

--- pelutils/types.py
+++ pelutils/types.py
@@ -5,5 +5,7 @@ import numpy.typing as npt
 
 # Common numpy types
 AnyArray = npt.NDArray[Any]  # pyright: ignore[reportExplicitAny]
+BoolArray = npt.NDArray[np.bool_]
+ComplexArray = npt.NDArray[np.complexfloating[Any]]  # pyright: ignore[reportExplicitAny, reportInvalidTypeArguments]
 FloatArray = npt.NDArray[np.floating[Any]]  # pyright: ignore[reportExplicitAny]
 IntArray = npt.NDArray[np.integer[Any]]  # pyright: ignore[reportExplicitAny]
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,14 +1,16 @@
 import numpy as np
 
-from pelutils.types import AnyArray, FloatArray, IntArray
+from pelutils import AnyArray, BoolArray, ComplexArray, FloatArray, IntArray
 
 
 def function_which_takes_np_types(
     any_array: AnyArray,
+    bool_array: BoolArray,
+    complex_array: ComplexArray,
     float_array: FloatArray,
     int_array: IntArray,
-) -> FloatArray:
-    return any_array + float_array + int_array
+) -> ComplexArray:
+    return any_array + bool_array + complex_array + float_array + int_array
 
 
 def test_types():
@@ -17,6 +19,8 @@ def test_types():
     Just make sure that using the types doesn't break anything silly like function declarations.
     """
     any_array = np.arange(5, dtype=np.float16)
+    bool_array = np.arange(5).astype(bool)
+    complex_array = 1j * np.arange(5)
     float_array = np.arange(5, dtype=np.float32)
     int_array = np.arange(5)
-    function_which_takes_np_types(any_array, float_array, int_array)
+    function_which_takes_np_types(any_array, bool_array, complex_array, float_array, int_array)
