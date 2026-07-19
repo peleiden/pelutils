6cc1e2ca07024e26b170b7700d87ec5cb68e808c
asgerius <asger.s@pm.me>
2026-07-19 22:34:33 +0200

    Create stand-alone array submodule

--- pelutils/array/_unique.py
+++ pelutils/array/_unique.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import ctypes
 from typing import TypeVar, cast
 
 import _pelutils_c as _c
@@ -15,29 +14,6 @@ pd = import_pandas()
 torch = import_torch()
 
 _ArrayT = TypeVar("_ArrayT", bound=npt.ArrayLike)
-
-
-def array_ptr(arr: npt.ArrayLike) -> ctypes.c_void_p:
-    """Return a pointer to a numpy array or torch tensor which can be used to interact with it in low-level languages like C/C++/Rust.
-
-    This function is mostly useful when not using Python's C api and instead interfacing with .so files directly with ctypes.
-    """
-    if torch is not None and isinstance(arr, torch.Tensor):
-        return ctypes.c_void_p(arr.data_ptr())
-    if not isinstance(arr, np.ndarray):
-        raise TypeError(f"Array should be of type np.ndarray or torch.Tensor, not {type(arr)}")
-    if not arr.flags.c_contiguous:
-        raise ValueError("Array must be C-contiguous")
-    return ctypes.c_void_p(arr.ctypes.data)
-
-
-def array_bytes(x: npt.ArrayLike) -> int:
-    """Calculate the size of a numpy array or torch tensor in bytes."""
-    if torch is not None and isinstance(x, torch.Tensor):
-        x = x.numpy()
-    if isinstance(x, np.ndarray):
-        return x.nbytes
-    raise TypeError(f"`x` of type {type(x)} is not a numpy array or torch tensor")
 
 
 def unique(  # noqa: PLR0912
