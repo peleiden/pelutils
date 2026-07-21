692c2142d568ad22765539e496a8503693fd08d9
asgerius <asger.s@pm.me>
2026-07-21 19:29:42 +0200

    Remove unused macro

--- pelutils/_c/array/blob.h
+++ pelutils/_c/array/blob.h
@@ -9,9 +9,6 @@
 #include "../hashmap/hashmap.h"
 #include "../types.h"
 
-/* Wrap signed x into the range [0, m), where m is positive and nonzero. */
-#define WRAP_MOD(x, m) ((((x) % ((i64)m)) + ((i64)m)) % ((i64)m))
-
 
 struct node_t {
     // ndim long array of representing a single coordinate in the grid
