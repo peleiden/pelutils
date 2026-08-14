b675f1f48aee6fddbd49872c395f5c9cb335c929
asgerius <asger.s@pm.me>
2026-08-14 22:29:17 +0200

    Add python 3.15 classifier
    Resolves #268

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3.15",
     ],
 )
 
